119a2b5d6014deffd89d1504844e9caa7354e323
Michał Ogiba <131243395+Mifiszon@users.noreply.github.com>
2025-03-21 13:46:46 +0100

    auth.php

--- api/autoryzacja.php
+++ api/autoryzacja.php
@@ -1,6 +1,6 @@
 <?php
-    $dbhost = 'localhost';
-    $dbuser = '22_ogiba';
-    $dbpass = 'Kropczka1@';
-    $dbname = '22_ogiba';
+    $dbhost = '';
+    $dbuser = '';
+    $dbpass = '@';
+    $dbname = '';
 ?>
